c1f119199d2051b24ac850231feca62778f395e8
Sean Sassenrath <sean.sassenrath@gmail.com>
2017-07-09 23:31:44 -0700

    Setup testing with Jest and TS

--- __tests__/index.ios.tsx
+++ __tests__/index.ios.tsx
@@ -1,6 +1,6 @@
 import 'react-native';
 import React from 'react';
-import Index from '../index.ios.js';
+import Index from '../src/index.ios';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
@@ -8,5 +8,6 @@ import renderer from 'react-test-renderer';
 it('renders correctly', () => {
   const tree = renderer.create(
     <Index />
-  );
-});
+  )
+  expect(tree).toBeDefined()
+})
